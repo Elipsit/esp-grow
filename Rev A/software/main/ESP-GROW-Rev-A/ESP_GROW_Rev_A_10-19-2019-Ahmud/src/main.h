93a39ad931ea543e997b31a53d089da79522d8d1
Elipsit <45702077+Elipsit@users.noreply.github.com>
2019-10-21 19:16:35 -0700

    updated ahmud code with new set point

--- Rev A/software/main/ESP-GROW-Rev-A/ESP_GROW_Rev_A_10-19-2019-Ahmud/src/main.h
+++ Rev A/software/main/ESP-GROW-Rev-A/ESP_GROW_Rev_A_10-19-2019-Ahmud/src/main.h
@@ -9,13 +9,13 @@ int soilsetpoint = 35;
 
 //Humm House Soil Sensor mapping scale measured when soil wet
 int sensmap_dry = 930;
-int sensmap_wet = 690;
+int sensmap_wet = 725;
 
 // Wifi Details
-const char* ssid = "Pretty Fly For A Wifi-2.4";
-const char* password = "supercarbon";
+//const char* ssid = "Pretty Fly For A Wifi-2.4";
+//const char* password = "supercarbon";
 
-//const char* ssid = "HUMMHQ";
-//const char* password = "humm1992";
+const char* ssid = "HUMMHQ";
+const char* password = "humm1992";
 
 #endif
